4f842caea608b9bd8e77501a3ad874f4646da9c8
EinsKatze <82060534+EinsKatze@users.noreply.github.com>
2023-01-26 12:07:01 +0100

    Switch to MB

--- BigFileGenerator.py
+++ BigFileGenerator.py
@@ -1,52 +1,58 @@
-import time
 import os
-import webbrowser
 import threading
+import time
+
 from colorama import Fore, Style
 
-GB= 1024*1024*1024
+MB = 1024 * 1024
 
-def typeAnim(text, speed, newLine = True):
-        for i in text:
-            print(i, end = "", flush = True)
-            time.sleep(speed)
-        if newLine:
-            print()
+
+def typeAnim(text, speed, newline=True):
+    for i in text:
+        print(i, end="", flush=True)
+        time.sleep(speed)
+    if newline:
+        print()
+
 
 def writeFile(file, lol):
-    for x in range(lol):
-        file.write(os.urandom(GB))
+    for n in range(lol):
+        file.write(os.urandom(MB))
 
-def progressBar(current, total, barLength = 50):
-        percent = float(current) * 100 / total
-        arrow   = '-' * int(percent/100 * barLength - 1) + '>'
-        spaces  = ' ' * (barLength - len(arrow))
-        print(Fore.LIGHTGREEN_EX + 'Progress: [%s%s] %d %%' % (arrow, spaces, percent), end='\r')
+
+def progressBar(current, total, barlen=50):
+    percent = float(current) * 100 / total
+    arrow = '-' * int(percent / 100 * barlen - 1) + '>'
+    spaces = ' ' * (barlen - len(arrow))
+    print(Fore.LIGHTGREEN_EX + 'Progress: [%s%s] %d %%' % (arrow, spaces, percent), end='\r')
+
 
 def threadfunc(choice):
     global file
     global progress
     progress = 0
     file = open("BigFile.txt", "wb")
-    for i in range(int(choice)):
+    for _ in range(int(choice)):
         writeFile(file, 1)
         progress += 1
         progressBar(progress, int(choice))
     time.sleep(1)
     os.system('cls')
     file.close()
-        
 
-os.system("title " + 'Big TXT File Generator by EinsKatze#0546')
-typeAnim(Fore.CYAN + "Wanna generate a Big File? How big should the file be? (in GB)", 0.05)
-choice = input('Gigabytes > ' + Style.RESET_ALL)
-if int(choice) == 0:
-    for i in range(3):
-        print(Fore.RED + "YOU CANT GENERATE A FILE THATS 0 GIGABYTES BIG\n" + Style.RESET_ALL)
-        webbrowser.open('https://youtu.be/g8vUPIx5gD8?t=1', new=2, autoraise=True)
-        time.sleep(0.5)
-    exit(0)
-x = threading.Thread(target=threadfunc, args=(choice,))
-x.start()
-typeAnim(Fore.YELLOW + "Generating file . . .", 0.025)
-print('\n')
+
+def main():
+    os.system("title " + 'Big File Generator by EinsKatze#0546')
+    typeAnim(Fore.CYAN + "Enter the file Size! ", 0.05)
+    choice = input('Megabytes > ' + Style.RESET_ALL)
+    if int(choice) == 0:
+        print("Invalid. Try again.")
+        main()
+    x = threading.Thread(target=threadfunc, args=(choice,))
+    x.start()
+    typeAnim(Fore.YELLOW + "Generating file . . .", 0.025)
+    print('\n')
+
+
+if __name__ == '__main__':
+    main()
