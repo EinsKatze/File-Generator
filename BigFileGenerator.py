2cd8f0edd36b537284e2be4192aaa6585467ba44
EinsKatze <82060534+EinsKatze@users.noreply.github.com>
2023-07-19 21:52:28 +0200

    Simplification

--- BigFileGenerator.py
+++ BigFileGenerator.py
@@ -1,21 +1,14 @@
 import os
-import threading
-import time
 
 MiB = 1024 * 1024
 
 
-def typeAnim(text, speed, newline=True):
-    for i in text:
-        print(i, end="", flush=True)
-        time.sleep(speed)
-    if newline:
-        print()
-
-
-def writeFile(file, lol):
-    for n in range(lol):
+def writeFile(file, size):
+    file = open(fName, "wb")
+    for n in range(int(size)):
         file.write(os.urandom(MiB))
+        progressBar(n, int(size))
+    file.close()
 
 
 def progressBar(current, total, barlen=50):
@@ -25,30 +18,17 @@ def progressBar(current, total, barlen=50):
     print('Progress: [%s%s] %d %%' % (arrow, spaces, percent), end='\r')
 
 
-def threadfunc(fName, choice):
-    global file
-    file = open(fName, "wb")
-    for progress in range(int(choice)):
-        writeFile(file, 1)
-        progressBar(progress, int(choice))
-    time.sleep(1)
-    os.system('cls')
-    file.close()
-
-
 def main():
-    os.system("title " + 'Big File Generator by EinsKatze²#9444')
-    typeAnim("Please enter the file name.", 0.05)
+    print("Please enter the file name.", 0.05)
     fName = input('Filename > ')
-    typeAnim("Please enter the file size.", 0.05)
+    print("Please enter the file size.", 0.05)
     choice = input('Mibibytes > ')
     if int(choice) <= 0:
         print("Invalid. Try again.")
         main()
-    writeFileThread = threading.Thread(target=threadfunc, args=(fName, choice,))
-    writeFileThread.start()
-    typeAnim("Generating file ...", 0.025)
-    print('\n')
+    print("Generating file ...\n\n")
+    writeFile(fName, choice)
+    print("\n\n")
 
 
 if __name__ == '__main__':
